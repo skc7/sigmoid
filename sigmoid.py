2325b023cad3fcd71eb5dac0e8904f4c925143bf
ksankisa <skc7893@hotmail.com>
2019-04-13 11:10:52 +0530

    moved graph plot to graph_plot method

--- sigmoid.py
+++ sigmoid.py
@@ -24,38 +24,25 @@ def grad_w(w,b,x,y) :
     fx = f(w,b,x)
     return (fx - y) * fx * (1 - fx) * x
 
-def do_gradient_descent() :
-    w, b, eta, max_epochs = -2, -2, 1.0, 1000
-    for i in range(max_epochs) :
-        dw, db = 0, 0
-        for x,y in zip(X,Y) :
-            dw = dw + grad_w(w,b,x,y)
-            db = db + grad_b(w,b,x,y)
-        w = w - eta * dw
-        b = b - eta * db
-
-def main():
+def graph_plot():
+    global ax
     ax = plt.axes(projection='3d')
     ax.set_xlabel('w')
     ax.set_ylabel('b')
     ax.set_zlabel('error')
-
     zline = np.linspace(-1, 1, 1000)
     xline = np.linspace(-10, 10, 1000)
     yline = np.linspace(-10, 10, 1000)
     ax.plot3D(xline, yline, zline, 'gray')
-
     xdata = list(np.arange(-10,10,0.2))
     ydata = list(np.arange(-10,10,0.2))
-
-
     A, B = np.meshgrid(xdata,ydata)
-
     for a, b in zip(A,B):
         ax.scatter(a,b,error(a,b), cmap='viridis')
 
-    #gradient descent 
-    w, b, eta, max_epochs = -2, -2, 0.5, 1000
+
+def do_gradient_descent(w, b, eta, max_epochs) :
+    graph_plot()
     for i in range(max_epochs) :
         dw, db = 0, 0
         for x,y in zip(X,Y) :
@@ -64,8 +51,13 @@ def main():
         w = w - eta * dw
         b = b - eta * db
         ax.scatter(w,b,error(w,b),marker='.',color='k')
-        ax.scatter(w,b,-3,marker='.',color='k')
+        ax.scatter(w,b,-1,marker='.',color='k')
     plt.show()
+
+
+def main():
+    w, b, eta, max_epochs = -2, -2, 1.0, 1000
+    do_gradient_descent(w, b, eta, max_epochs)
 
 
 if __name__ == "__main__":
