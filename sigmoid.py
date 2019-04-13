b191fb4f724ffda82dd574008f9b30e0faf11631
ksankisa <skc7893@hotmail.com>
2019-04-13 12:06:18 +0530

    added do_momentum_gradient_descent

--- sigmoid.py
+++ sigmoid.py
@@ -34,8 +34,8 @@ def graph_plot():
     xline = np.linspace(-10, 10, 1000)
     yline = np.linspace(-10, 10, 1000)
     ax.plot3D(xline, yline, zline, 'gray')
-    xdata = list(np.arange(-10,10,0.2))
-    ydata = list(np.arange(-10,10,0.2))
+    xdata = list(np.arange(-10,10,0.5))
+    ydata = list(np.arange(-10,10,0.5))
     A, B = np.meshgrid(xdata,ydata)
     for a, b in zip(A,B):
         ax.scatter(a,b,error(a,b), cmap='viridis')
@@ -54,9 +54,28 @@ def do_gradient_descent(w, b, eta, max_epochs) :
         ax.scatter(w,b,-1,marker='.',color='k')
     plt.show()
 
+def do_momentum_gradient_descent(w,b,eta,max_epochs):
+    graph_plot()
+    prev_v_w, prev_v_b, gamma = 0, 0, 0.2
+    for i in range(max_epochs):
+        dw, db = 0, 0
+        for x, y in zip(X,Y):
+            dw = dw + grad_w(w,b,x,y)
+            db = db + grad_b(w,b,x,y)
+
+        v_w = gamma*prev_v_w + eta*dw
+        v_b = gamma*prev_v_b + eta*db
+        w = w - v_w
+        b = b - v_b
+        ax.scatter(w,b,error(w,b),marker='.',color='k')
+        ax.scatter(w,b,-1,marker='.',color='k')
+        prev_v_w = v_w
+        prev_v_b = v_b
+    plt.show()
 
 def main():
     w, b, eta, max_epochs = -2, -2, 1.0, 1000
+    do_momentum_gradient_descent(w, b, eta, max_epochs)
     do_gradient_descent(w, b, eta, max_epochs)
 
 
